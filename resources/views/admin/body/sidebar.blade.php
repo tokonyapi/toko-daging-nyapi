fd5fd23f60f44ff33ccbefe08781e23df280989a
tokonyapi <tokonyapi@gmail.com>
2024-08-16 17:41:03 +0700

    update tampilan dan penambahan asset

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -4,7 +4,7 @@
 
         <div class="user-profile">
 			<div class="ulogo">
-				 <a href="index.html">
+				 <a href="{{ route('admin.dashboard') }}">
 				  <!-- logo for regular state and mobile devices -->
 					 <div class="d-flex align-items-center justify-content-center">
 						  {{-- <img src="{{ asset('assets/logo3.png') }}" width="70" alt=""> --}}
